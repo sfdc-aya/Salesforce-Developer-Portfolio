8ca35da73acffedc8a48277a0be81f4d645b80d8
Aya <62959544+amukhammedova@users.noreply.github.com>
2023-09-14 11:16:59 -0700

    Update project-lwc-bmi-calculator.js

--- project-lwc-bmi-calculator.js
+++ project-lwc-bmi-calculator.js
@@ -1,1 +1,25 @@
-project-lwc-bmi-calculator.html
+import { LightningElement, track } from 'lwc';
+
+export default class BmiCalculator extends LightningElement {
+   @track weight = '';
+   @track height = '';
+   @track bmi = '';
+
+    weightChange(event){
+        this.weight = parseFloat(event.target.value);
+    }
+
+    heightChange(event){
+        this.height = parseFloat(event.target.value);
+    }
+
+    calculateBMI(){
+        this.bmi = ((this.weight / (this.height * this.height)) * 10000).toFixed(1);
+    }
+
+    resetFields(event){
+        this.weight = '';
+        this.height = '';
+        this.bmi = '';
+    }
+}
