a1ee6490c30379c6052b84108464d01edc8c7d75
Aya <62959544+amukhammedova@users.noreply.github.com>
2023-09-18 11:55:34 -0700

    Update checkBoxGroupChild.js

--- checkBoxGroupChild.js
+++ checkBoxGroupChild.js
@@ -1,1 +1,33 @@
+import { LightningElement, track, api } from 'lwc';
 
+export default class PublicMethodChild extends LightningElement {
+    @track value = ['red'];
+
+    options = [
+        { label: 'Yellow Marker', value: 'yellow' },
+        { label: 'Gold Marker', value: 'gold' },
+        { label: 'Green Marker', value: 'green' },
+        { label: 'Black Marker', value: 'black' },
+        { label: 'Blue Marker', value: 'blue' },
+        { label: 'White Marker', value: 'white' },
+        { label: 'Purple Marker', value: 'purple' },
+        { label: 'Grey Marker', value: 'grey' },
+        { label: 'Red Marker', value: 'red' },
+    ];
+
+
+    @api
+    selectCheckbox(checkboxValue){
+        const selectedCheckbox = this.options.find( checkbox =>{
+            return checkboxValue === checkbox.value;
+        })
+
+        if(selectedCheckbox){
+            this.value = selectedCheckbox.value;
+            return "Successfully checked";
+        } 
+        return "No checkbox found";
+        
+    }
+
+}
